50d887312f9431b0c058cf0ac9ad9efc233b9793
nhatminhpuf <51844984+nhatminhpuf@users.noreply.github.com>
2022-04-27 09:44:35 +0700

    Add files via upload

---
+++ js/mapdata.js
@@ -0,0 +1,132 @@
+var mapData = {
+  type: 'FeatureCollection',
+  features: [{
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-77.038659, 38.931567]
+      },
+      properties: {
+        title: 'Hiring Online English Dial Women Jewelrychers',
+        price: '1,200$/Month',
+        bed: 3,
+        bath: 2,
+        size: 2000 + ' sqft',
+        thumbnail: 'assets/img/products/1.jpg',
+        description: 'Washington, D.C.'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-77.003168, 38.894651]
+      },
+      properties: {
+        title: 'Development Team Lead',
+        price: '1,200$/Month',
+        bed: 3,
+        bath: 2,
+        size: 2000 + ' sqft',
+        thumbnail: 'assets/img/products/2.jpg',
+        description: 'San Francisco, California'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-77.090372, 38.881189]
+      },
+      properties: {
+        title: 'Hiring Online English Dial Women Jewelrychers',
+        price: '1,200$/Month',
+        bed: 3,
+        bath: 2,
+        size: 2000 + ' sqft',
+        thumbnail: 'assets/img/products/3.jpg',
+        description: 'San Francisco, California'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-77.111561, 38.882342]
+      },
+      properties: {
+        title: 'Development Team Lead',
+        price: '1,200$/Month',
+        bed: 3,
+        bath: 2,
+        size: 2000 + ' sqft',
+        thumbnail: 'assets/img/products/4.jpg',
+        description: 'San Francisco, California'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-77.052477, 38.943951]
+      },
+      properties: {
+        title: 'Hiring Online English Dial Women Jewelrychers',
+        price: '1,200$/Month',
+        bed: 3,
+        bath: 2,
+        size: 2000 + ' sqft',
+        thumbnail: 'assets/img/products/5.jpg',
+        description: 'San Francisco, California'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-77.043444, 38.909664]
+      },
+      properties: {
+        title: 'Development Team Lead',
+        price: '1,200$/Month',
+        bed: 3,
+        bath: 2,
+        size: 2000 + ' sqft',
+        thumbnail: 'assets/img/products/6.jpg',
+        description: 'San Francisco, California'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-77.031706, 38.914581]
+      },
+      properties: {
+        title: 'Hiring Online English Dial Women Jewelrychers',
+        price: '1,200$/Month',
+        bed: 3,
+        bath: 2,
+        size: 2000 + ' sqft',
+        thumbnail: 'assets/img/products/7.jpg',
+        description: 'San Francisco, California'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-77.020945, 38.878241]
+      },
+      properties: {
+        title: 'Development Team Lead',
+        price: '1,200$/Month',
+        bed: 3,
+        bath: 2,
+        size: 2000 + ' sqft',
+        thumbnail: 'assets/img/products/8.jpg',
+        description: 'San Francisco, California'
+      }
+    },
+  ]
+};
